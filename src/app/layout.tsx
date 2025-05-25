459327c1afc2b9eec66bfb853b30bbe678d3bb14
winuss <niceyss@gmail.com>
2025-05-25 20:24:54 +0900

    feat:ad script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next';
+import Script from 'next/script';
 
 import { Toaster } from '@/components/ui/toaster';
 import { baseDomain, blogDesc, blogName, blogThumbnailURL } from '@/config/const';
@@ -40,6 +41,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko' className='h-full scroll-smooth' suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1154659137489563"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className='flex min-h-screen flex-col font-pretendard'>
         <ThemeProvider>
           <Header />
